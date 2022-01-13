d6ea05ee833cdbf23cc78093c0cab204a466c62f
saravanak <smurugesan@VA-VAT10384.vsa.com>
2022-01-13 20:04:29 +0530

    ReverseNumberExample2.java

---
+++ java-examples/ReverseNumberExample2.java
@@ -0,0 +1,15 @@
+public class ReverseNumberExample2   
+{  
+public static void main(String[] args)   
+{  
+int number = 123456, reverse = 0;  
+//we have not mentioned the initialization part of the for loop  
+for( ;number != 0; number=number/10)   
+{  
+int remainder = number % 10;  
+reverse = reverse * 10 + remainder;  
+}  
+System.out.println(" reverse of the given number using for loop"); 
+System.out.println("The reverse of the given number is: " + reverse);  
+}  
+}  
